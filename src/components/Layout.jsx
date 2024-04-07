5d78782000b261f384626eb8f411d8b680c5e2d2
Kevin Havn <kevin-havn@hotmail.com>
2024-04-07 19:38:54 +0200

    added flex and justify-content-space-between to fix footer positioning

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable react/prop-types */
-
-import { Header } from "./Header.jsx";
-import { Footer } from "./Footer.jsx";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Layout({ children }) {
 	return (
-		<>
+		<div className="d-flex flex-column min-vh-100">
 			<Header />
-			<main>{children}</main>
+			<main className="flex-grow-1">{children}</main>
 			<Footer />
-		</>
+		</div>
 	);
 }
 
