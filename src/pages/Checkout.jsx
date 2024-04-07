82aa1c182aa70cdf58bfb2573595ff2fe6587082
Kevin Havn <kevin-havn@hotmail.com>
2024-04-07 21:15:48 +0200

    fixed contact page console.log(), fixed error with checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -4,31 +4,31 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 import { useCart } from "../components/CartContext";
 
 function Checkout() {
-  const { updateCart } = useCart();
+	const { updateCart } = useCart();
 
-  useEffect(() => {
-    updateCart([]);
-  }, [updateCart]);
+	useEffect(() => {
+		updateCart([]);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return (
-    <Container
-      className="d-flex justify-content-center align-items-center"
-      style={{ minHeight: "100vh" }}>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Body className="text-center">
-              <Card.Title>Success!</Card.Title>
-              <Card.Text>Your checkout process was successful.</Card.Text>
-              <Card.Text>
-                Interested in browsing more? <Link to="/">Click here!</Link>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
+	return (
+		<Container
+			className="d-flex justify-content-center align-items-center"
+			style={{ minHeight: "100vh" }}>
+			<Row>
+				<Col>
+					<Card>
+						<Card.Body className="text-center">
+							<Card.Title>Success!</Card.Title>
+							<Card.Text>Your checkout process was successful.</Card.Text>
+							<Card.Text>
+								Interested in browsing more? <Link to="/">Click here!</Link>
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
-
 export default Checkout;
