7648b168c0d0f22076a5ee239b1c3343397ec480
Kevin Havn <kevin-havn@hotmail.com>
2024-04-07 19:38:33 +0200

    made footer & header functions default

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,14 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export function Footer() {
+function Footer() {
 	return (
 		<footer>
 			<Navbar expand="lg" className="bg-body-tertiary">
 				<Container>
-					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="me-auto">
-							<Link className="nav-link" to="../about">About Us</Link>
 							<Link className="nav-link" to="../contact">Contact Us</Link>
 						</Nav>
 					</Navbar.Collapse>
@@ -19,3 +17,5 @@ export function Footer() {
 		</footer>
 	);
 }
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,21 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import CartIcon from "./CartIcon";
 import { Link } from "react-router-dom";
 
-export function Header() {
+function Header() {
 	return (
 		<header>
 			<Navbar expand="lg" className="bg-body-tertiary">
 				<Container>
-					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Brand>Javascript Frameworks Course Assignment</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="me-auto">
-							<Link className="nav-link" to="../Home">
-								Home
-							</Link>
 							<Link className="nav-link" to="../Products">
 								Products
 							</Link>
-							<Link className="nav-link" to="../Cart"><CartIcon /></Link>
+							<Link className="nav-link" to="../Cart">
+								<CartIcon />
+							</Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
@@ -25,3 +24,5 @@ export function Header() {
 		</header>
 	);
 }
+
+export default Header;
