daa76fbbab30fa63c662414b38b863efdf150a12
Kevin Havn <kevin-havn@hotmail.com>
2024-04-07 19:37:24 +0200

    added visually hidden label

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/prop-types */
 function SearchBar({ onSearchChange }) {
-    return (
-      <input
-        className="form-control mb-3"
-        type="text"
-        placeholder="Search products..."
-        onChange={(e) => onSearchChange(e.target.value)}
-      />
-    );
-  }
-  
-  export default SearchBar;
-  
+	return (
+		<div>
+			<label htmlFor="searchProducts" className="visually-hidden">
+				Search Products
+			</label>
+			<input
+				id="searchProducts"
+				className="form-control mb-3"
+				type="text"
+				placeholder="Search products..."
+				onChange={(e) => onSearchChange(e.target.value)}
+			/>
+		</div>
+	);
+}
+
+export default SearchBar;
