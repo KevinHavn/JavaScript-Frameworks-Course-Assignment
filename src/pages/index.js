d9b7e02558d0041c969cb1beabad27f9c11546e2
Kevin Havn <kevin-havn@hotmail.com>
2024-04-07 19:39:35 +0200

    swapped home page for products page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as Home } from './Home';
-export { default as Products } from './Products';
+export { default as Home } from './Products';
 export { default as Cart } from './Cart';
 export { default as Contact } from './Contact';
+export { default as ProductDetailPage } from "./ProductDetailPage"
