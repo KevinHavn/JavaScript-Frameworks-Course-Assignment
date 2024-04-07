4b87b9d7532de402419e5bc18d3aca9d828ecbda
Kevin Havn <kevin-havn@hotmail.com>
2024-04-07 19:38:12 +0200

    fixed routing, added flex to fix footer positioning - added description to html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Layout from "./components/Layout";
 import { CartProvider } from "./components/CartContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, Products, Cart, Contact } from "./pages";
+import { Home, Cart, Contact, ProductDetailPage } from "./pages";
 import "./styles/custom.scss";
 import About from "./pages/About";
 
@@ -12,7 +12,7 @@ function App() {
 				<Layout>
 					<Routes>
 						<Route path="/home" element={<Home />} />
-						<Route path="/products" element={<Products />} />
+						<Route path="/product/:productId" element={<ProductDetailPage />} />
 						<Route path="/cart" element={<Cart />} />
 						<Route path="/about" element={<About />} />
 						<Route path="/contact" element={<Contact />} />
